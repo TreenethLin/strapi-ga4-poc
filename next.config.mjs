fb2fd2d7652317ac14dca390322938880b421bed
Yuttapichai Paul Linglom <yuttapichai.chanchalamep@outlook.com>
2024-09-12 10:38:50 +0700

    fix-nextConfig

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-      domains: ['negixttyakfaxiigxfxi.supabase.co'],
-    },
+        remotePatterns: [
+          {
+            protocol: 'https',
+            hostname: 'negixttyakfaxiigxfxi.supabase.co',
+            pathname: '**',
+          },
+        ],
+      },
     env: {
       STRAPI_URL: process.env.NEXT_PUBLIC_STRAPI_URL,
       STRAPI_API_TOKEN: process.env.STRAPI_API_TOKEN,
